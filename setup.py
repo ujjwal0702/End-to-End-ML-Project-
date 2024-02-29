b096d2bf68956ff0390661e7aec30f1eb34cb3f5
ujjwal0702 <ujjwal.srivastava0702>
2024-02-29 12:42:18 +0530

    commit second

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "End-to-end-ML-Project-Implementation"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "ujjwal0702"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "ujjwal.srivastava0702@gmail.com"
 
 
 setuptools.setup(
